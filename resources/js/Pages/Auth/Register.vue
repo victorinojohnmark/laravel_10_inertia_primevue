59a79f2597e8bbcc5ba0b7759479a58b92b0bec4
victorinojohnmark <victorinojohnmark@gmail.com>
2024-10-26 11:03:02 +0800

    Fix css conflict

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -94,7 +94,7 @@ onMounted(() => {
             <div class="flex justify-end items-center">
                 <Link
                     :href="route('login')"
-                    class="mr-4 text-muted-color underline text-muted-color hover:text-color"
+                    class="mr-4 underline text-muted-color hover:text-color"
                 >
                     Already registered?
                 </Link>
