17a9c1c4c9aa2ab5a8f9e96782f51546976abb06
victorinojohnmark <victorinojohnmark@gmail.com>
2024-10-28 10:29:20 +0800

    Adjustment on inputerror element

--- resources/js/Components/InputError.vue
+++ resources/js/Components/InputError.vue
@@ -8,8 +8,8 @@ defineProps({
 
 <template>
     <div v-show="message">
-        <p class="text-sm text-red-500 dark:text-red-300">
+        <small class="text-xs text-red-500 dark:text-red-300">
             {{ message }}
-        </p>
+        </small>
     </div>
 </template>
